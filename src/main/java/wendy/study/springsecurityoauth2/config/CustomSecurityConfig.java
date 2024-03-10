2dbdbfc4b1463ab1d16ebe68ba325ff7a00cfb53
beeguriri <kkamboting@gmail.com>
2024-03-10 16:46:24 +0900

    AuthenticationEntryPoint custom

--- src/main/java/wendy/study/springsecurityoauth2/config/CustomSecurityConfig.java
+++ src/main/java/wendy/study/springsecurityoauth2/config/CustomSecurityConfig.java
@@ -3,6 +3,7 @@ package wendy.study.springsecurityoauth2.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
@@ -13,11 +14,15 @@ public class CustomSecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeRequests().anyRequest().authenticated();
         http.httpBasic();
-        http.formLogin();
-        http.exceptionHandling().authenticationEntryPoint(
-            (request, response, authException) -> {
-                System.out.println("custom Entry Point");
-        });
+        http.httpBasic().authenticationEntryPoint(new CustomAuthenticationEntryPoint());
+        //spring security default : 인증정보 session 사용
+        //session을 사용하지 않겠다고 선언하면 매 요청마다 인증받아야 함
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+//        http.formLogin();
+//        http.exceptionHandling().authenticationEntryPoint(
+//            (request, response, authException) -> {
+//                System.out.println("custom Entry Point");
+//        });
         return http.build();
     }
 
